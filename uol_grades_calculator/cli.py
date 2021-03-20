5d4d638ef05937ee49c0d3b85ced41d42def626c
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-03-19 21:55:37 -0600

    Move commands into commands.py
    * Keep programming logic separate from cli.py, which just lists the
      commands available in a succinct way.
    * Fixes #11.

--- uol_grades_calculator/cli.py
+++ uol_grades_calculator/cli.py
@@ -4,12 +4,12 @@ Describes the commands available from the terminal when running this tool.
 
 # Standard library imports
 from pathlib import Path
-import pprint
 
 # Third-party library imports
 import click
 
 # Local imports
+from uol_grades_calculator import commands
 from uol_grades_calculator.grades import Grades
 
 
@@ -31,33 +31,4 @@ def cli(ctx, config):
 @cli.command()
 @pass_grades
 def summarize(grades):
-    """Print a summary of the progress made so far."""
-    prettyp = pprint.PrettyPrinter(indent=2)
-    print("Modules taken:")
-    prettyp.pprint(grades.get_list_of_finished_modules())
-    print("Number of modules done:", grades.get_num_of_finished_modules())
-    print("Scores so far:", grades.get_module_scores_of_finished_modules())
-    print(
-        f"\nWeighted average: {grades.weighted_average}"
-        f" (ECTS: {grades.get_ects_equivalent_score(grades.weighted_average)},"
-        f" US: {grades.get_us_letter_equivalent_score(grades.weighted_average)})"
-    )
-    print(
-        f"Unweighted average: {grades.unweighted_average}"
-        f" (ECTS: {grades.get_ects_equivalent_score(grades.unweighted_average)},"
-        f" US: {grades.get_us_letter_equivalent_score(grades.unweighted_average)})"
-    )
-    print("\nClassification:", grades.get_classification())
-    print("\nECTS grade equivalence:")
-    prettyp.pprint(
-        grades.get_module_scores_of_finished_modules_for_system(system="ECTS")
-    )
-    print("\nUS grade equivalence:")
-    prettyp.pprint(
-        grades.get_module_scores_of_finished_modules_for_system(system="US")
-    )
-    print(f"\nGPA: {grades.get_us_gpa()} (US) – {grades.get_uk_gpa()} (UK)")
-    print(
-        f"Total credits done: {grades.get_total_credits()} / 360",
-        f"({grades.get_percentage_degree_done()}%)",
-    )
+    commands.summarize(grades)
---
+++ uol_grades_calculator/commands.py
@@ -0,0 +1,39 @@
+"""
+List the commands available from the CLI: one per function.
+"""
+
+# Standard library imports
+import pprint
+
+
+def summarize(grades):
+    """Print a summary of the progress made so far."""
+    prettyp = pprint.PrettyPrinter(indent=2)
+    print("Modules taken:")
+    prettyp.pprint(grades.get_list_of_finished_modules())
+    print("Number of modules done:", grades.get_num_of_finished_modules())
+    print("Scores so far:", grades.get_module_scores_of_finished_modules())
+    print(
+        f"\nWeighted average: {grades.weighted_average}"
+        f" (ECTS: {grades.get_ects_equivalent_score(grades.weighted_average)},"
+        f" US: {grades.get_us_letter_equivalent_score(grades.weighted_average)})"
+    )
+    print(
+        f"Unweighted average: {grades.unweighted_average}"
+        f" (ECTS: {grades.get_ects_equivalent_score(grades.unweighted_average)},"
+        f" US: {grades.get_us_letter_equivalent_score(grades.unweighted_average)})"
+    )
+    print("\nClassification:", grades.get_classification())
+    print("\nECTS grade equivalence:")
+    prettyp.pprint(
+        grades.get_module_scores_of_finished_modules_for_system(system="ECTS")
+    )
+    print("\nUS grade equivalence:")
+    prettyp.pprint(
+        grades.get_module_scores_of_finished_modules_for_system(system="US")
+    )
+    print(f"\nGPA: {grades.get_us_gpa()} (US) – {grades.get_uk_gpa()} (UK)")
+    print(
+        f"Total credits done: {grades.get_total_credits()} / 360",
+        f"({grades.get_percentage_degree_done()}%)",
+    )
