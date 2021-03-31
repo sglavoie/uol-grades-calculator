078f3e671f968cc171e7a1ea14b7e0d9450475e8
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-03-30 20:17:55 -0600

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="uol-grades-calculator",
-    version="0.2.0",
+    version="0.3.0",
     description="Grades calculator for the BSc Computer Science at the University of London",
     long_description=README,
     long_description_content_type="text/markdown",
