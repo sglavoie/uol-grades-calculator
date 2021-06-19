sglavoie/uol-grades-calculator

commit 26c2750fa39146fd84a1816c6bbbae7611e43532

Test config against missing modules and invalid syntax

ugc/config.py
@@ -17,7 +17,7 @@ class Config:
     """Loads the configuration file where grades are stored."""
 
     def __init__(self, config_path=None):
-        self.config = None
+        self.config = {}
         if config_path is not None:
             self.path = config_path
         else:
@@ -26,32 +26,58 @@ class Config:
     def load(self) -> dict:
         """Load grades from a YAML file."""
         try:
-            with open(self.path) as gfile:
-                self.config = yaml.safe_load(gfile)
-                self.verify()
-                return self.config
+            open(self.path)
         except FileNotFoundError:
             click.secho(
                 f"Configuration file not found: {self.path}", fg="bright_red"
             )
-            return {}
+        with open(self.path) as gfile:
+            self.check_config_format_is_syntactically_correct()
+            self.config = yaml.safe_load(gfile)
+            self.verify()
+            return self.config
 
-    def verify(self) -> bool:
-        """Check that the config file contains valid data.
-        Return True when it's valid, False otherwise."""
-        conditions = [self.check_total_weight_sums_up_100_in_all_modules()]
-        return all(conditions)
+    def verify(self) -> None:
+        """Check that the config file contains valid data. One of the
+        functions will throw an error if the config is not valid."""
+        self.check_total_weight_sums_up_100_in_all_modules()
+        self.all_modules_are_found_with_valid_names()
+        self.all_modules_are_set_to_correct_level()
+
+    def check_config_format_is_syntactically_correct(self) -> bool:
+        """Just make it much less probable we will find garbage in an
+        altered config. Avoids having to check for YAML syntax errors."""
+        grades_template = Path(__file__).parent / "grades-template.yml"
+        with open(grades_template) as tfile:
+            expected_start_lines = tfile.read().splitlines()
+        expected_start_lines[1:] = [
+            l.split(":")[0] + ":" for l in expected_start_lines[1:]
+        ]
+
+        with open(self.path) as gfile:
+            content = gfile.read().splitlines()
+
+        for idx, line in enumerate(content):
+            if not line.startswith(expected_start_lines[idx]):
+                raise ConfigValidationError(
+                    f"Line {idx + 1} does not match "
+                    f"the template. Expected '{expected_start_lines[idx]}', "
+                    f"got '{line}'"
+                )
+        return True
 
     def check_total_weight_sums_up_100_in_all_modules(self) -> bool:
         for module, values in self.config.items():
-            if not self.check_total_weight_sums_up_100_for_module(
+            if not self._check_total_weight_sums_up_100_for_module(
                 values, module
             ):
                 return False
         return True
 
     @staticmethod
-    def check_total_weight_sums_up_100_for_module(module, module_name) -> bool:
+    def _check_total_weight_sums_up_100_for_module(
+        module, module_name
+    ) -> bool:
         if not module.get("final_weight") and not module.get("midterm_weight"):
             return True  # missing both is OK
 
@@ -81,4 +107,85 @@ class Config:
                 f"the module {module_name}"
             )
 
+        return True
+
+    def all_modules_are_found_with_valid_names(self) -> bool:
+        all_modules = [
+            "Algorithms and Data Structures I",
+            "Computational Mathematics",
+            "Discrete Mathematics",
+            "Fundamentals of Computer Science",
+            "How Computers Work",
+            "Introduction to Programming I",
+            "Introduction to Programming II",
+            "Web Development",
+            "Algorithms and Data Structures II",
+            "Agile Software Projects",
+            "Computer Security",
+            "Databases Networks and the Web",
+            "Graphics Programming",
+            "Object Oriented Programming",
+            "Programming with Data",
+            "Software Design and Development",
+            "Databases and Advanced Data Techniques",
+            "Machine Learning and Neural Networks",
+            "Artificial Intelligence",
+            "Intelligent Signal Processing",
+            "Natural Language Processing",
+            "Data Science",
+            "Final Project",
+        ]
+        for module in all_modules:
+            if (
+                module not in self.config.keys()
+                and module != "Numerical Mathematics"
+            ):
+                raise ConfigValidationError(
+                    f"Module '{module}' not found in configuration file"
+                    f"({self.path}). Make sure the spelling is correct."
+                )
+        for module in self.config.keys():
+            if module not in all_modules:
+                raise ConfigValidationError(
+                    f"Module '{module}' not expected in"
+                    f"configuration file ({self.path})."
+                )
+        return True
+
+    def all_modules_are_set_to_correct_level(self):
+        levels = {
+            "Algorithms and Data Structures I": 4,
+            "Computational Mathematics": 4,
+            "Discrete Mathematics": 4,
+            "Fundamentals of Computer Science": 4,
+            "How Computers Work": 4,
+            "Introduction to Programming I": 4,
+            "Introduction to Programming II": 4,
+            "Web Development": 4,
+            "Agile Software Projects": 5,
+            "Algorithms and Data Structures II": 5,
+            "Computer Security": 5,
+            "Databases Networks and the Web": 5,
+            "Graphics Programming": 5,
+            "Object Oriented Programming": 5,
+            "Programming with Data": 5,
+            "Software Design and Development": 5,
+            "Artificial Intelligence": 6,
+            "Data Science": 6,
+            "Databases and Advanced Data Techniques": 6,
+            "Intelligent Signal Processing": 6,
+            "Machine Learning and Neural Networks": 6,
+            "Natural Language Processing": 6,
+            "Final Project": 6,
+        }
+        for module, values in self.config.items():
+            if (
+                values is None
+                or not values.get("level")
+                or values["level"] != levels[module]
+            ):
+                raise ConfigValidationError(
+                    f"Module '{module}' contains an invalid level value "
+                    f"(expected '{levels[module]}')."
+                )
         return True

tests/conftest.py
@@ -5,12 +5,15 @@ Set up pytest fixtures for convenient testing.
 # Third-party library imports
 import pathlib
 import pytest
+import yaml
 
 # Local imports
 from ugc.config import Config
 from ugc.grades import Grades
 
 FIXTURE_GRADES_PATH = "tests/fixtures/yaml/grades.yml"
+FIXTURE_BAD_CONFIG_PATH = "tests/fixtures/yaml/bad_format.yml"
+FIXTURE_GRADES_LEVELS_PATH = "tests/fixtures/yaml/levels.yml"
 FIXTURE_PARTIAL_GRADES_INACCURATE_PATH = (
     "tests/fixtures/yaml/partial_grades_inaccurate.yml"
 )
@@ -28,6 +31,13 @@ def local_grades():
 
 
 @pytest.fixture(scope="module")
+def grades_levels():
+    """Return a dict containing all the modules with their respective level."""
+    with open(FIXTURE_GRADES_LEVELS_PATH) as lfile:
+        return yaml.safe_load(lfile)
+
+
+@pytest.fixture(scope="module")
 def local_partial_grades_inaccurate():
     """Return an instance of the Grades class as a fixture with few UoL modules."""
     config_path = (
@@ -41,6 +51,13 @@ def local_config():
     """Return an instance of the Config class as a fixture available
     for the function."""
     config_path = pathlib.Path().absolute() / FIXTURE_GRADES_PATH
+    return Config(config_path=config_path)
+
+@pytest.fixture(scope="function")
+def local_bad_config():
+    """Return an instance of the Config class as a fixture available
+    for the function."""
+    config_path = pathlib.Path().absolute() / FIXTURE_BAD_CONFIG_PATH
     return Config(config_path=config_path)
 
 

tests/fixtures/yaml/levels.yml
@@ -1,12 +1,47 @@
+---
 Algorithms and Data Structures I:
   level: 4
+Computational Mathematics:
+  level: 4
 Discrete Mathematics:
+  level: 4
+Fundamentals of Computer Science:
+  level: 4
+How Computers Work:
+  level: 4
+Introduction to Programming I:
+  level: 4
+Introduction to Programming II:
+  level: 4
+Web Development:
   level: 4
 Algorithms and Data Structures II:
   level: 5
 Agile Software Projects:
   level: 5
+Computer Security:
+  level: 5
+Databases Networks and the Web:
+  level: 5
+Graphics Programming:
+  level: 5
+Object Oriented Programming:
+  level: 5
+Programming with Data:
+  level: 5
+Software Design and Development:
+  level: 5
 Databases and Advanced Data Techniques:
+  level: 6
+Machine Learning and Neural Networks:
+  level: 6
+Artificial Intelligence:
+  level: 6
+Intelligent Signal Processing:
+  level: 6
+Natural Language Processing:
+  level: 6
+Data Science:
   level: 6
 Final Project:
   level: 6

tests/fixtures/yaml/modules_in_progress.yml
@@ -1,65 +1,185 @@
 ---
-Module 1: # valid
+Algorithms and Data Structures I: # valid
+  completion_date:
   final_score: 65
   final_weight: 70
   midterm_score: 79
   midterm_weight: 30
   module_score:
   level: 4
-Module 2: # valid
-  final_score: 60
-  final_weight: 50
-  midterm_score:
-  midterm_weight: 50
-  module_score:
-  level: 5
-Module 3: # valid
-  final_score:
-  final_weight: 50
-  midterm_score: 75
-  midterm_weight: 50
-  module_score:
-  level: 6
-Module 4: # invalid: module_score should be empty
-  final_score:
-  final_weight: 50
-  midterm_score: 60
-  midterm_weight: 50
-  module_score: 80
-  level: 4
-Module 5: # invalid: module_score should be empty
-  final_score: 80
-  final_weight: 50
-  midterm_score: 60
-  midterm_weight: 50
-  module_score: 82
-  level: 4
-Module 6: # invalid: module_score should be empty
+Computational Mathematics: # invalid: module_score should be empty
+  completion_date:
   final_score: 80
   final_weight: 50
   midterm_score:
   midterm_weight: 50
   module_score: 85
   level: 4
-Module 7: # invalid: not enough data
-  level: 4
-Module 8: # invalid: not enough data
-  module_score:
-Module 9: # invalid: missing "level"
+Discrete Mathematics:
+  completion_date:
   final_score:
-  final_weight: 50
-  midterm_score: 60
-  midterm_weight: 50
-  module_score:
-Module 10: # invalid: missing "level"
-  final_score: 60
   final_weight: 50
   midterm_score:
   midterm_weight: 50
   module_score:
-Module 11: # invalid: missing "level"
-  final_score: 70
+  level: 4
+Fundamentals of Computer Science:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 4
+How Computers Work: # invalid: module_score should be empty
+  completion_date:
+  final_score:
   final_weight: 50
   midterm_score: 60
   midterm_weight: 50
+  module_score: 80
+  level: 4
+Introduction to Programming I: # invalid: module_score should be empty
+  completion_date:
+  final_score: 80
+  final_weight: 50
+  midterm_score: 60
+  midterm_weight: 50
+  module_score: 82
+  level: 4
+Introduction to Programming II: # invalid: not enough data
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
   module_score:
+  level: 4
+Web Development: # invalid: not enough data
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
+  level: 4
+Agile Software Projects: # valid
+  completion_date:
+  final_score:
+  final_weight: 70
+  midterm_score: 60
+  midterm_weight: 30
+  module_score:
+  level: 5
+Algorithms and Data Structures II: # valid
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score: 75
+  midterm_weight: 50
+  module_score:
+  level: 5
+Computer Security:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Databases Networks and the Web:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Graphics Programming:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Object Oriented Programming:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Programming with Data:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Software Design and Development:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Artificial Intelligence:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
+  level: 6
+Data Science:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
+Databases and Advanced Data Techniques:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
+Intelligent Signal Processing:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
+  level: 6
+Machine Learning and Neural Networks:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
+Natural Language Processing:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
+  level: 6
+Final Project:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
+  level: 6

tests/fixtures/yaml/partial_grades_inaccurate.yml
@@ -7,6 +7,54 @@ Algorithms and Data Structures I:
   midterm_weight: 50
   module_score: 88
   level: 4
+Computational Mathematics:
+  completion_date: 2019-09
+  final_score: 61
+  final_weight: 50
+  midterm_score: 99
+  midterm_weight: 50
+  module_score: 80
+  level: 4
+Discrete Mathematics:
+  completion_date: 2020-03
+  final_score: 100
+  final_weight: 50
+  midterm_score: 99
+  midterm_weight: 50
+  module_score: 100
+  level: 4
+Fundamentals of Computer Science:
+  completion_date: 2020-09
+  final_score: 98
+  final_weight: 50
+  midterm_score: 98
+  midterm_weight: 50
+  module_score: 98
+  level: 4
+How Computers Work:
+  completion_date: 2018-12
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score: -1
+  level: 4
+Introduction to Programming I:
+  completion_date: 2019-09
+  final_score: 100
+  final_weight: 50
+  midterm_score: 100
+  midterm_weight: 50
+  module_score: 100
+  level: 4
+Introduction to Programming II:
+  completion_date: 2020-03
+  final_score: 98
+  final_weight: 70
+  midterm_score: 100
+  midterm_weight: 30
+  module_score: 99
+  level: 4
 Web Development:
   completion_date: 2020-09
   final_score: 90
@@ -14,21 +62,77 @@ Web Development:
   midterm_score: 61
   midterm_weight: 50
   module_score: 77
-  level: 5
-Machine Learning and Neural Networks:
+  level: 4
+Agile Software Projects:
   completion_date: 2021-03
-  final_score: 72
+  final_score: 91
   final_weight: 70
-  midterm_score: 86.5
+  midterm_score: 74
   midterm_weight: 30
-  module_score: 76
-  level: 6
-Intelligent Signal Processing:
+  module_score:
+  level: 5
+Algorithms and Data Structures II:
+  completion_date: 2020-09
+  final_score: 92
+  final_weight: 50
+  midterm_score: 92
+  midterm_weight: 50
+  module_score: 92
+  level: 5
+Computer Security:
   completion_date:
   final_score:
-  final_weight: 70
+  final_weight: 50
   midterm_score:
-  midterm_weight: 30
+  midterm_weight: 50
+  module_score:
+  level: 5
+Databases Networks and the Web:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Graphics Programming:
+  completion_date: 2021-03
+  final_score: 100
+  final_weight: 50
+  midterm_score: 96
+  midterm_weight: 50
+  module_score:
+  level: 5
+Object Oriented Programming:
+  completion_date: 2020-09
+  final_score: 96
+  final_weight: 50
+  midterm_score: 96
+  midterm_weight: 50
+  module_score: 96
+  level: 5
+Programming with Data:
+  completion_date: 2021-03
+  final_score:
+  final_weight: 50
+  midterm_score: 84
+  midterm_weight: 50
+  module_score:
+  level: 5
+Software Design and Development:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 5
+Artificial Intelligence:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
   module_score:
   level: 6
 Data Science:
@@ -38,4 +142,44 @@ Data Science:
   midterm_score:
   midterm_weight: 30
   module_score: 79
+  level: 6
+Databases and Advanced Data Techniques:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
+Intelligent Signal Processing:
+  completion_date:
+  final_score:
+  final_weight: 70
+  midterm_score:
+  midterm_weight: 30
+  module_score:
+  level: 6
+Machine Learning and Neural Networks:
+  completion_date: 2021-03
+  final_score: 72
+  final_weight: 70
+  midterm_score: 86.5
+  midterm_weight: 30
+  module_score: 76
+  level: 6
+Natural Language Processing:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
+  level: 6
+Final Project:
+  completion_date:
+  final_score:
+  final_weight:
+  midterm_score:
+  midterm_weight:
+  module_score:
   level: 6

tests/test_config.py
@@ -29,6 +29,11 @@ def test_grades_yml_is_loaded_as_dict(local_config):
     assert isinstance(data, dict)
 
 
+def test_bad_format_grades_yml_raises_ConfigValidationError(local_bad_config):
+    with pytest.raises(ConfigValidationError):
+        local_bad_config.load()
+
+
 @pytest.mark.parametrize(
     "final_weight,midterm_weight,expected_value",
     [
@@ -44,7 +49,7 @@ def test_check_total_weight_sums_up_100(
         "midterm_weight": midterm_weight,
     }
     assert (
-        local_config.check_total_weight_sums_up_100_for_module(module, "name")
+        local_config._check_total_weight_sums_up_100_for_module(module, "name")
         == expected_value
     )
 
@@ -69,7 +74,7 @@ def test_check_total_weight_sums_up_100_raises_error(
             "final_weight": final_weight,
             "midterm_weight": midterm_weight,
         }
-        assert local_config.check_total_weight_sums_up_100_for_module(
+        assert local_config._check_total_weight_sums_up_100_for_module(
             module, "module_name"
         )
 
@@ -97,3 +102,45 @@ def test_check_total_weight_sums_up_100_all_modules(local_config):
     }
     local_config.config = config_true_2
     assert local_config.check_total_weight_sums_up_100_in_all_modules()
+
+
+def test_all_modules_are_found_with_valid_names_returns_True(
+    local_config, grades_levels
+):
+    local_config.config = grades_levels
+    assert local_config.all_modules_are_found_with_valid_names()
+
+
+def test_all_modules_are_found_with_missing_modules_raises_ConfigValidationError(
+    local_config, grades_levels
+):
+    with pytest.raises(ConfigValidationError):
+        local_config.config = grades_levels.copy()
+        local_config.config.pop("Final Project")  # missing one module
+        # print(config_false_1)
+        local_config.all_modules_are_found_with_valid_names()
+
+
+def test_all_modules_are_found_with_extra_modules_raises_ConfigValidationError(
+    local_config, grades_levels
+):
+    with pytest.raises(ConfigValidationError):
+        local_config.config = grades_levels.copy()
+        local_config.config["Module 24"] = {}  # nonexistent module
+        local_config.all_modules_are_found_with_valid_names()
+
+
+def test_all_modules_are_set_to_correct_level_returns_True(
+    local_config, grades_levels
+):
+    local_config.config = grades_levels
+    assert local_config.all_modules_are_set_to_correct_level()
+
+
+def test_all_modules_are_set_to_correct_level_raises_ConfigValidationError_on_missing(
+    local_config, grades_levels
+):
+    with pytest.raises(ConfigValidationError):
+        local_config.config = grades_levels
+        local_config.config["Discrete Mathematics"] = None
+        local_config.all_modules_are_set_to_correct_level()

tests/test_grades.py
@@ -59,7 +59,7 @@ class TestDataIsRetrievedCorrectly:
     def test_get_list_of_modules_in_progress(grades_modules_in_progress):
         expected_list = [
             {
-                "Module 1": {
+                "Algorithms and Data Structures I": {
                     "final_score": 65,
                     "final_weight": 70,
                     "midterm_score": 79,
@@ -67,12 +67,18 @@ class TestDataIsRetrievedCorrectly:
                     "level": 4,
                 }
             },
-            {"Module 2": {"final_score": 60, "final_weight": 50, "level": 5}},
             {
-                "Module 3": {
+                "Agile Software Projects": {
+                    "midterm_score": 60,
+                    "midterm_weight": 30,
+                    "level": 5,
+                }
+            },
+            {
+                "Algorithms and Data Structures II": {
                     "midterm_score": 75,
                     "midterm_weight": 50,
-                    "level": 6,
+                    "level": 5,
                 }
             },
         ]
@@ -215,12 +221,12 @@ class TestDataIsCalculatedWell:
         grades_modules_in_progress,
     ):
         # finished : [80, 82, 85], respectively [L4, L4, L4]
-        # in progress: [69.2, 60, 75], respectively [L4, L5, L6]
-        # weighted average of all that: 72.60
+        # in progress: [69.2, 75, 60], respectively [L4, L5, L5]
+        # weighted average of all that: 72.12
         result = (
             grades_modules_in_progress.calculate_weighted_average_in_progress()
         )
-        assert result == 72.60
+        assert result == 72.12
 
     @staticmethod
     def test_get_module_scores_of_finished_modules_for_system_us(local_grades):

ugc/grades-template.yml
@@ -7,6 +7,14 @@ Algorithms and Data Structures I:
   midterm_weight: 50
   module_score:
   level: 4
+Computational Mathematics:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 4
 Discrete Mathematics:
   completion_date:
   final_score:
@@ -39,14 +47,6 @@ Introduction to Programming I:
   midterm_weight: 50
   module_score:
   level: 4
-Numerical Mathematics:
-  completion_date:
-  final_score:
-  final_weight: 50
-  midterm_score:
-  midterm_weight: 50
-  module_score:
-  level: 4
 Introduction to Programming II:
   completion_date:
   final_score:
@@ -63,20 +63,20 @@ Web Development:
   midterm_weight: 30
   module_score:
   level: 4
-Algorithms and Data Structures II:
-  completion_date:
-  final_score:
-  final_weight: 50
-  midterm_score:
-  midterm_weight: 50
-  module_score:
-  level: 5
 Agile Software Projects:
   completion_date:
   final_score:
   final_weight: 70
   midterm_score:
   midterm_weight: 30
+  module_score:
+  level: 5
+Algorithms and Data Structures II:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
   module_score:
   level: 5
 Computer Security:
@@ -87,7 +87,7 @@ Computer Security:
   midterm_weight: 50
   module_score:
   level: 5
-Databases networks and the web:
+Databases Networks and the Web:
   completion_date:
   final_score:
   final_weight: 50
@@ -127,7 +127,31 @@ Software Design and Development:
   midterm_weight: 50
   module_score:
   level: 5
+Artificial Intelligence:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
+Data Science:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
 Databases and Advanced Data Techniques:
+  completion_date:
+  final_score:
+  final_weight: 50
+  midterm_score:
+  midterm_weight: 50
+  module_score:
+  level: 6
+Intelligent Signal Processing:
   completion_date:
   final_score:
   final_weight: 50
@@ -143,15 +167,7 @@ Machine Learning and Neural Networks:
   midterm_weight: 50
   module_score:
   level: 6
-Artificial Intelligence:
-  completion_date:
-  final_score:
-  final_weight:
-  midterm_score:
-  midterm_weight:
-  module_score:
-  level: 6
-Intelligent Signal Processing:
+Natural Language Processing:
   completion_date:
   final_score:
   final_weight: 50
@@ -159,27 +175,11 @@ Intelligent Signal Processing:
   midterm_weight: 50
   module_score:
   level: 6
-Natural Language Processing:
-  completion_date:
-  final_score:
-  final_weight:
-  midterm_score:
-  midterm_weight:
-  module_score:
-  level: 6
-Elective Module:
-  completion_date:
-  final_score:
-  final_weight:
-  midterm_score:
-  midterm_weight:
-  module_score:
-  level: 6
 Final Project:
   completion_date:
   final_score:
-  final_weight:
+  final_weight: 50
   midterm_score:
-  midterm_weight:
+  midterm_weight: 50
   module_score:
   level: 6

commit f01f6679e8d449238f742eaef03f4ec78277c530

Fix bug introduced in 26c2750
* Fixes #25

ugc/__init__.py
@@ -1,2 +1,2 @@
 # Version of UoL Grades Calculator
-__version__ = "0.6.0"
+__version__ = "0.6.2"

ugc/config.py
@@ -37,10 +37,11 @@ class Config:
         """Load grades from a YAML file."""
         try:
             open(self.path)
-        except FileNotFoundError:
+        except FileNotFoundError as e:
             click.secho(
                 f"Configuration file not found: {self.path}", fg="bright_red"
             )
+            raise e
 
         with open(self.path) as gfile:
             self.check_config_format_is_syntactically_correct()

ugc/grades.py
@@ -19,6 +19,16 @@ class Grades:
     def __init__(self, config_path=None) -> None:
         """Set some default values before loading any grades."""
         self.config = Config(config_path=config_path)
+
+        # Return before raising an error with a config file not found
+        # so we can run the `generate-sample` command.
+        # Otherwise, trying to load the config file will unsurprisingly
+        # not work...
+        try:
+            self.config.load()
+        except FileNotFoundError:
+            return
+
         self.data = self.config.load()
         self.total_credits = 0
 