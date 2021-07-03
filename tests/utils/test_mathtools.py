a184a4a9a6d873e4157953ccc4f91731ca601db3
Sébastien Lavoie <sgdlavoie@gmail.com>
2021-07-03 09:37:10 -0500

    Test mathtools with Hypothesis
    * Ref #16

--- tests/utils/test_mathtools.py
+++ tests/utils/test_mathtools.py
@@ -3,6 +3,8 @@ Test utils/mathtools.py
 """
 
 # Third-party library imports
+from hypothesis import given, settings
+from hypothesis.strategies import integers, floats
 import pytest
 
 # Local imports
@@ -24,3 +26,37 @@ from ugc.utils import mathtools
 def test_round_half_up(number, decimals, expected_value):
     result = mathtools.round_half_up(number, decimals)
     assert result == expected_value
+
+
+@given(integers(), integers(min_value=11))
+def test_hypothesis_round_half_up_with_integers_raises_ValueError_on_large_decimals(
+    number, decimals
+):
+    with pytest.raises(ValueError):
+        mathtools.round_half_up(number, decimals)
+
+
+@given(integers(), integers(max_value=-1))
+def test_hypothesis_round_half_up_with_integers_and_negative_decimals(
+    number, decimals
+):
+    with pytest.raises(ValueError):
+        mathtools.round_half_up(number, decimals)
+
+
+@given(integers(max_value=1_000_000), integers(min_value=0, max_value=10))
+@settings(max_examples=10)
+def test_hypothesis_round_half_up_with_integers(number, decimals):
+    mathtools.round_half_up(number, decimals)
+
+
+@given(
+    floats(min_value=1_000_001, max_value=1_000_001),
+    integers(min_value=0, max_value=10),
+)
+@settings(max_examples=10)
+def test_hypothesis_round_half_up_with_floats_raises_ValueError_on_large_number(
+    number, decimals
+):
+    with pytest.raises(ValueError):
+        mathtools.round_half_up(number, decimals)
--- ugc/utils/mathtools.py
+++ ugc/utils/mathtools.py
@@ -4,7 +4,15 @@ Math helper functions.
 import math
 
 
-def round_half_up(num: float, decimals=0):
-    """Round a float up and return it."""
+def round_half_up(num: float, decimals: int = 0):
+    """Round a float up and return it.
+
+    Assumes 10 decimals is enough precision. Also assumes we won't be rounding
+    anything beyond 1,000,000 as that's far outside the range of expected
+    values and would lead to overflow errors in this implementation."""
+    if decimals > 10 or decimals < 0:
+        raise ValueError("Supporting up to 10 decimals of precision, no more!")
+    if num > 1_000_000:
+        raise ValueError("We don't round that high around here (max value: 1,000,000)")
     multiplier = 10 ** decimals
     return math.floor(num * multiplier + 0.5) / multiplier
